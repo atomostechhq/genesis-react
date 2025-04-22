70d3e9fe7590aa03a627f90c02ff45e97b28f0d4
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2025-04-22 16:22:24 +0530

    changes rollback

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import Test from "./Test";
 import Tabletest from "./Tabletest";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <div>
-        <h1 className="text-brand-500">Atomos Genesis React</h1>
-        <Test />
-        <Tabletest />
-      </div>
-    </BrowserRouter>
+    <div>
+      <h1 className="text-brand-500">Atomos Genesis React</h1>
+      <Test />
+      <Tabletest />
+    </div>
   );
 }
 
